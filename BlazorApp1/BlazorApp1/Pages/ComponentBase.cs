2481f633f5bb93d278a1171adf918638cbe085b9
Ahmedcreadev <73081603+Ahmedcreadev@users.noreply.github.com>
2021-01-11 17:32:22 +0200

    test
    test

---
+++ BlazorApp1/BlazorApp1/Pages/ComponentBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp1.Pages
+{
+    public class ComponentBase : Microsoft.AspNetCore.Components.ComponentBase
+    {
+        protected int currentCount = 0;
+
+        protected void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+}
